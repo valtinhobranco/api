95c36b6eefc5c8177ebe9de49d5a595d0191e1e2
Valter Branco Filho <valtinhobranco89@gmail.com>
2024-11-09 16:00:00 -0300

    production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv'
 
-if ( process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'test') {
   config({
     path: '.env.test',
   })
@@ -12,15 +12,16 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success ===  false) {
-    console.error('⚠ Invalid environment variables', _env.error.format())
-    throw new Error('⚠  Invalid environment variables')
+if (_env.success === false) {
+  console.error('⚠ Invalid environment variables', _env.error.format())
+  throw new Error('⚠  Invalid environment variables')
 }
 
 export const env = _env.data
